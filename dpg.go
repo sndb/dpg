f3cc967a67f0e85ace6c7982cb991ffd73e60e7f
sndb <sndb@sndb.xyz>
2021-06-20 23:20:43 +0300

    replace switch w/ WordListShorthands map; add ErrInvalidWordList; logger setup at init(); refactor

--- dpg.go
+++ dpg.go
@@ -8,6 +8,10 @@ import (
 	"github.com/sndb/dpg/dwgen"
 )
 
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
 func main() {
 	wordCount := flag.Int("c", 6, "word count")
 	sourceList := flag.String("l", "l", "source word list ('l': long word list (for use with five dice), 's': general short word list (for use with four dice), 'u3cp': short word list (with words that have unique three-character prefixes)")
@@ -15,12 +19,12 @@ func main() {
 	number := flag.Int("n", 1, "number of passphrases to generate")
 	flag.Parse()
 
-	config := &dwgen.Config{
+	config := dwgen.Config{
 		WordCount:  *wordCount,
 		SourceList: *sourceList,
 		Delimiter:  *delimiter,
 	}
-	g, err := dwgen.New(config)
+	g, err := dwgen.New(&config)
 	if err != nil {
 		log.Fatal(err)
 	}
--- dwgen/dwgen.go
+++ dwgen/dwgen.go
@@ -2,56 +2,68 @@ package dwgen
 
 import (
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"log"
 	"math/big"
 	"strings"
 )
 
-func r(max int) int64 {
-	x, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return x.Int64()
+// WordListShorthands is the map of the list shorthands to the list pointers
+var WordListShorthands = map[string]*WordList{
+	"l":    &LongWordList,
+	"s":    &ShortWordList,
+	"u3cp": &UniquePrefixShortWordList,
 }
 
-// WordList holds a list of diceware words and the number of dice to be thrown
-// for indexing the list
+// DefaultConfig is the default Config used by Generator
+var DefaultConfig = Config{
+	WordCount:  6,
+	SourceList: "l",
+	Delimiter:  " ",
+}
+
+// ErrInvalidWordList is returned by New to indicate that an invalid word list
+// shorthand is used
+var ErrInvalidWordList = errors.New("not a valid word list")
+
+// WordList holds the list of diceware words and the number of dice to be
+// thrown for indexing the list
 type WordList struct {
-	List    map[int]string
-	DiceNum int
+	List       map[int]string
+	DiceNumber int
 }
 
-// Config holds parameters for Generator
+// Config holds the settings for Generator
 type Config struct {
-	WordCount  int
+	// WordCount is the amount of words used to generate passphrase
+	WordCount int
+
+	// SourceList is the word list used to get words from
 	SourceList string
-	Delimiter  string
+
+	// Delimiter is the delimiter between separate words
+	Delimiter string
 }
 
-// Generator generates passphrases
+// Generator uses Config and WordList to generate passphrases
 type Generator struct {
 	*Config
 	*WordList
 }
 
-// New creates a new Generator
+// New returns a new Generator
 func New(config *Config) (*Generator, error) {
-	var l *WordList
-	switch config.SourceList {
-	case "l":
-		l = &LongWordList
-	case "s":
-		l = &ShortWordList
-	case "u3cp":
-		l = &UniquePrefixShortWordList
-	default:
-		return nil, fmt.Errorf("%q is not a valid word list", config.SourceList)
+	if config == nil {
+		config = &DefaultConfig
+	}
+	wordList, ok := WordListShorthands[config.SourceList]
+	if !ok {
+		return nil, fmt.Errorf("%q: %w", config.SourceList, ErrInvalidWordList)
 	}
 	g := Generator{
 		Config:   config,
-		WordList: l,
+		WordList: wordList,
 	}
 	return &g, nil
 }
@@ -60,18 +72,26 @@ func New(config *Config) (*Generator, error) {
 func (g *Generator) Generate() (string, error) {
 	var p []string
 	for i := 0; i < g.Config.WordCount; i++ {
-		p = append(p, g.WordList.List[Dice(g.WordList.DiceNum)])
+		p = append(p, g.WordList.List[Dice(g.WordList.DiceNumber)])
 	}
 	return strings.Join(p, g.Config.Delimiter), nil
 }
 
 // Dice throws n dice
 func Dice(n int) int {
-	var d int
+	d := 0
 	for i := 0; i < n; i++ {
 		d *= 10
 		x := r(6) + 1
 		d += int(x)
 	}
 	return d
+}
+
+func r(max int) int64 {
+	x, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return x.Int64()
 }
